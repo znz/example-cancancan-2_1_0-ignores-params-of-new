ca8dfae6ef0ffc1c0a4d8a0bcee8a2b24de9cd0e
Kazuhiro NISHIYAMA <zn@mbf.nifty.com>
2017-11-13 16:09:19 +0900

    Test preset params

--- test/system/posts_test.rb
+++ test/system/posts_test.rb
@@ -6,4 +6,9 @@ class PostsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "Post"
   # end
+
+  test "visit new with param" do
+    visit new_post_url(post: { title: 'Preset' })
+    assert has_field?('Title', with: 'Preset')
+  end
 end
